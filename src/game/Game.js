4933b3b35412dd7b802513e1efe0422d3e27e6b9
LucasPinottiBrito <lucaspinottibrito@gmail.com>
2024-05-19 00:51:56 -0300

    car is movinggit add .

--- src/game/Game.js
+++ src/game/Game.js
@@ -1,26 +1,40 @@
-import { Stage, Sprite, Container, Text } from "@pixi/react"
-import car from "./car.png"
-import React, {useState} from "react"
+import React, { useEffect, useState } from "react";
+import { Button, Image } from "antd";
+import car from "../kenney_racing-pack/PNG/Cars/car_black_5.png";
+import street from "../kenney_racing-pack/PNG/Tiles/Asphalt road/road_asphalt02.png";
+import "./animation.css"
 
-export default function Game(){ 
-    const [speed, setSpeed] = useState(1.1)
-    const [positionX, setPositionX] = useState(100)
+export default function Game() {
+    const [isMoving, setIsMoving] = useState(false)
+    const [time, setTime] = useState('3')
 
-    return(
-        <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
-            <Stage options={{ background: 0xe1e1e1 }}>
-                <Container x={400} y={150}>
-                    <Text text="The Speed Game" anchor={{ x: 0.5, y: 0.5 }}/>
-                </Container>
-                <Sprite
-                    image={car}
-                    x={positionX}
-                    y={270}
-                    anchor={{ x: 0.5, y: 0.5 }}
-                    angle={90}
-                    
-                />
-            </Stage>
+    const renderScenario = ()=>{
+        let streetNum = 8
+        let streetImgs = []
+        for(let i = 0; i<streetNum; i++){
+            streetImgs.push(<Image src={street} preview={false} style={{left: 128*i}}/>)
+        }
+        return streetImgs
+    }
+
+    return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '700px', width: '100%' }}>
+      <div style={{position: 'relative', height: '40%', width: '1100px', margin: '10px', display: 'flex', alignItems: 'center' }}>
+        <div style={{position: 'absolute', zIndex: 1, left: 0}}>
+            {renderScenario()}
         </div>
-    )
+        <div className={`car ${isMoving ? 'moving' : ''}` } style={{
+            transition: `transform ${isMoving ? time : '0.5'}s linear`
+        }}>
+            <Image src={car} preview={false} />
+        </div>
+      </div>
+      {(isMoving==false)
+      ?
+        <Button onClick={()=>{setIsMoving(true)}}>Correr</Button>
+      :
+        <Button onClick={()=>{setIsMoving(false)}}>Retornar</Button>
+      }
+    </div>
+  );
 }
